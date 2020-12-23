dddc020c0b116e399d56b2389192f2757cd914d0
CorentinVachon <corentin.vachon@itycom.com>
2020-12-23 17:26:48 +0100

    create first base

--- playMonopoly.py
+++ playMonopoly.py
@@ -3,15 +3,36 @@ if __name__ == "__main__":
 
 class Game:
     pass
-    #TODO : define game var like :
+    #TODO : define game var like : duration, nb of turn
+
+class BoardGame:
+    pass
+    #TODO : sort the card by the order
+
+class Case:
+    pass
+    #TODO : sum every occurences
 
 class CardEffect:
     pass
+    #TODO : define type (chance, community), effect and target
 
 class CardTerritory:
     pass
+    #TODO : define color, average price and position
 
-
+class CardStack:
+    pass
+    #TODO : create buffer circular functions to simulate stack of cards
 
 def play():
     pass
+
+def throwDices():
+    pass
+    #TODO : generate 2 randint between 1 and 6
+
+def movePlayer():
+    pass
+    #TODO : change the position of the player
+
